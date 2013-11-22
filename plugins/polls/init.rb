07f932c915e502a8f8fe99ed8680c650834582e6
RVladislav <Vladislav.Razhnov@gmail.com>
2013-11-22 09:35:00 +0400

    a little bit

--- plugins/polls/init.rb
+++ plugins/polls/init.rb
@@ -3,5 +3,5 @@ Redmine::Plugin.register :polls do
   author 'Vladislav R.'
   description 'Question & Answer'
   version '0.0.1'
-  menu :application_menu, :polls, { :controller => 'polls', :action => 'index' }, :caption => 'Polls'
+  menu :application_menu, :polls, { :controller => 'polls', :action => 'index' }, :caption => 'Pools'
 end
