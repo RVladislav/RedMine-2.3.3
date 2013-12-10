3282e9489b39051efe22a07e1b0fcfe2a6b03ae6
RVladislav <Vladislav.Razhnov@gmail.com>
2013-12-10 04:31:59 +0400

    del another collumns

--- plugins/quest/db/migrate/1100000000000000000002_create_answers.rb
+++ plugins/quest/db/migrate/1100000000000000000002_create_answers.rb
@@ -2,8 +2,6 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
 
-      t.integer :idQuestions
-
       t.string :textAnswer
 
       t.belongs_to :questions
--- plugins/quest/db/migrate/1100000000000000000003_create_useranswers.rb
+++ plugins/quest/db/migrate/1100000000000000000003_create_useranswers.rb
@@ -2,10 +2,6 @@ class CreateUseranswers < ActiveRecord::Migration
   def change
     create_table :useranswers do |t|
 
-      t.integer :idUsers
-
-      t.integer :idAnswer
-
       t.belongs_to :users
 
       t.belongs_to :answers
